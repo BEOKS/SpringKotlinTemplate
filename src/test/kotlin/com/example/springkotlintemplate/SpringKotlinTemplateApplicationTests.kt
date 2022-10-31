502d2e32bb32ed4a048c50688cd9ecb1342663d2
BEOKS <lee01042000@gmail.com>
2022-11-01 00:33:32 +0900

    Chore : add jacoco test code and exclude SpringBootApplication class

--- src/test/kotlin/com/example/springkotlintemplate/SpringKotlinTemplateApplicationTests.kt
+++
@@ -1,9 +0,0 @@
-package com.example.springkotlintemplate
-
-import io.kotest.core.spec.style.DescribeSpec
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class SpringKotlinTemplateApplicationTests: DescribeSpec({
-
-})
