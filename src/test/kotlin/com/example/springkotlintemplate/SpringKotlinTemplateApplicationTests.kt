eb8a48a6a8853418c618f059e64363d8872807cc
BEOKS <lee01042000@gmail.com>
2022-10-31 14:15:30 +0900

    Chore : add mockk, kotest dependency

--- src/test/kotlin/com/example/springkotlintemplate/SpringKotlinTemplateApplicationTests.kt
+++ src/test/kotlin/com/example/springkotlintemplate/SpringKotlinTemplateApplicationTests.kt
@@ -1,13 +1,9 @@
 package com.example.springkotlintemplate
 
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.DescribeSpec
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringKotlinTemplateApplicationTests {
+class SpringKotlinTemplateApplicationTests: DescribeSpec({
 
-    @Test
-    fun contextLoads() {
-    }
-
-}
+})
